eb14e2f7ef4a4a108c5000e17c6f5f90a9649659
Vlad Indikt <vlad.indikt@gmail.com>
2020-03-19 18:38:15 +0200

    added login cmp

---
+++ src/views/login-signup-page.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class=login-signup-container>
+      <section class="user-login">
+            <form @submit.prevent="login">
+                <input type="text" placeholder="Name" v-model="credentials.username" />
+                <input type="password" placeholder="Your Password" v-model="credentials.password" />
+                <button>Login</button>
+            </form>
+        </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name:'login-signup-cmp',
+
+}
+</script>
+
+<style>
+
+</style>
