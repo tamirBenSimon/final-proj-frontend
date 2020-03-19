de29891318ef3bb66f26e8df22f74e38509b93cc
TamirBenSimon <tamirbssb@gmail.com>
2020-03-19 17:08:05 +0200

    dvd

---
+++ src/modules/artworkStore.js
@@ -0,0 +1,72 @@
+import artworkService from '../services/artwork.service.js'
+
+// var localLoggedinUser = null;
+// if (sessionStorage.user) localLoggedinUser = JSON.parse(sessionStorage.user);
+// loggedinUser : localLoggedinUser,
+
+export default {
+    state: {
+        artworks: []
+    },
+    getters: {
+        artworks(state) {
+            return state.artworks;
+        },
+
+    },
+    mutations: {
+        // setUser(state, {user}) {
+        //     state.loggedinUser = user;
+        // },
+        setArtworks(state, {artworks}) {
+            state.artworks = artworks;
+        },
+        removeArtwork(state, {artworkId}) {
+            state.artworks = state.artworks.filter(currArtwork => currArtwork._id !== artworkId)
+        },
+    },
+    actions: {
+
+        async loadartworks(context) {
+            const users = await artworkService.query();
+            context.commit({type: 'setArtworks', artworks})
+            return users;
+        },
+        async removeArtwork(context, {artworkId}) {
+            await artworkService.remove(artworkId);
+            context.commit({type: 'removeArtwork', artworkId})
+        },
+        async updateArtwork(context, {artwork}) {
+            artwork = await artworkService.update(artwork);
+            context.commit({type: 'setUser', artwork})
+            return artwork;
+        }
+    }
+}
+
+
+
+
+
+//action:
+        // async login(context, {userCred}) {
+        //     const user = await artworkService.login(userCred);
+        //     context.commit({type: 'setUser', user})
+        //     return user;
+        // },
+        // async signup(context, {userCred}) {
+        //     const user = await artworkService.signup(userCred)
+        //     context.commit({type: 'setUser', user})
+        //     return user;  
+        // },
+        // async logout(context) {
+        //     await artworkService.logout()
+        //     context.commit({type: 'setUsers', users: []})
+        //     context.commit({type: 'setUser', user: null})
+        // },
+
+// getter:
+        // loggedinUser(state) {
+        //     return state.loggedinUser
+        // }
+
--- src/store.js
+++ src/store.js
@@ -1,18 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import UserStore from './modules/UserStore.js'
-import SocketStore from './modules/SocketStore.js'
-import ReviewStore from './modules/ReviewStore.js'
+// import UserStore from './modules/UserStore.js'
+// import SocketStore from './modules/SocketStore.js'
+// import ReviewStore from './modules/ReviewStore.js'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: true,
   modules: {
-    UserStore,
-    SocketStore,
-    ReviewStore
+
 },
   state: {
 
