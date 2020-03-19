eb59c727b6dc079a18fbd9c09c77fecee238cce5
TamirBenSimon <tamirbssb@gmail.com>
2020-03-19 11:32:56 +0200

    גהג

---
+++ src/services/CloudinaryService.js
@@ -0,0 +1,24 @@
+export default {
+    uploadImg
+}
+
+function uploadImg(ev) {
+    const CLOUD_NAME = "ddarfzyr2"
+    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+
+    const formData = new FormData();
+    formData.append('file', ev.target.files[0])
+    formData.append('upload_preset', 'a5icujx4');
+
+    return fetch(UPLOAD_URL, {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            return res
+        })
+        .catch(err => console.error(error))
+}
+
---
+++ src/services/ReviewService.js
@@ -0,0 +1,38 @@
+import HttpService from './HttpService'
+
+export default {
+    add,
+    query,
+    remove
+}
+
+function query() {
+
+    // The filterBy will come from a parameter (Refactor this).
+    const filterBy = {
+        txt: 'a',
+        min: 8
+    }
+
+    // We create an instance of URLSearchParams
+    var queryParams = new URLSearchParams()
+
+    // Setting parameters in our queryParams (if they exist):
+    if (filterBy.txt) {
+        queryParams.set('txt', filterBy.txt)
+        queryParams.set('min', filterBy.min)
+    }
+
+    // Stringifying the queryParams to the "review?" (query params will always come after "?");
+    return HttpService.get(`review?${queryParams}`)
+}
+
+function remove(reviewId) {
+    return HttpService.delete(`review/${reviewId}`)
+}
+function add(review) {
+    return HttpService.post(`review`, review)
+}
+
+
+
