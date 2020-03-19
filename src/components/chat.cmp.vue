137b0c6fe016fa818345a748cec83d5e82f0fb89
Your Nameariel <arielzissu98@gmail.com>
2020-03-19 14:06:14 +0200

    ariel2

--- src/components/chat.cmp.vue
+++ src/components/chat.cmp.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>hey freind! hey douches nghnfgn</h1>
+  <section>
+    <h1>hey freind! hey douches nghnfgn</h1>
+    <h2>bla</h2>
+    <h3>ariel2</h3>
+    <h4>asdasd</h4>
+  </section>
 </template>
 
 <script>
