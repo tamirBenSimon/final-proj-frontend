a652093797622686193829c25359559a9030e40c
TamirBenSimon <tamirbssb@gmail.com>
2020-03-20 10:38:22 +0200

    hi zissu

--- src/components/product-list.cmp.vue
+++ src/components/product-list.cmp.vue
@@ -4,6 +4,9 @@
     <div class="artwork-list-artwork-prevs-container">
     <productPrev v-for="artwork in artworks" :key="artwork._id" :artwork="artwork" />
     </div>
+    <pre>
+      {{artworks[0]}}
+      </pre>
   </section>
 </template>
 
--- src/components/product-prev.cmp.vue
+++ src/components/product-prev.cmp.vue
@@ -1,40 +1,29 @@
 <template>
   <div class="artwork-prev">
-    <div class="artwork-prev-prev-img-container">
-      <img :src="artwork.imgURLs[0]" alt="">
+    <div class="artwork-prev-img-container">
+      <img :src="artwork.imgURLs[0]" alt="" />
     </div>
-    <h3>{{artwork.title}}</h3>
-    <span>{{artwork.desc}}</span> <br>
-    <span>price: {{artwork.price}}</span> <br>
-    <span>{{isInStock}}</span> <br>
-    <hr>
-    <h2>
-      </h2>
-    <span>{{}}</span> <br>
-
-    
-    <hr>
-    <pre>
-      {{artwork}}
-    </pre>
+    <div class="artwork-prev-txt-container">
+      <h3>{{ artwork.title }}</h3>
+      <span>{{ artwork.desc }}</span> <br />
+      <span>price: {{ artwork.price }}</span> <br />
+      <span>{{ isInStock }}</span> <br />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'product-prev',
-   props:{
-     artwork: Object
-   },
-   computed:{
-     isInStock(){
-       return this.artwork.inStock? 'available': 'currently not available'
-     }
-   }
-  
-}
+  name: "product-prev",
+  props: {
+    artwork: Object
+  },
+  computed: {
+    isInStock() {
+      return this.artwork.inStock ? "available" : "currently not available";
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
