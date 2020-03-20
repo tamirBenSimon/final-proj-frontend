ef64699e4812943efd2d4d094af9cb5d75001f4d
TamirBenSimon <tamirbssb@gmail.com>
2020-03-20 14:38:04 +0200

    x

--- src/services/event-bus.service.js
+++ src/services/event-bus.service.js
@@ -2,14 +2,3 @@ export const EVENT_PUK = 'puk';
 export const EVENT_REMOVE = 'remove';
 import Vue from 'vue';
 export const eventBus = new Vue()
-
-// eventBus.$on(EVENT_PUK, (num) => {
-//     console.log('Got a Puk!', num);
-
-//     const msg = {
-//         txt: 'Got a Puk!',
-//         type: 'danger'
-//     }
-
-//     eventBus.$emit(EVENT_SHOW_MSG, msg)
-// })
