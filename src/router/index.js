c5c83a806b5a026f825e661ab88e20173ba82a20
Vlad Indikt <vlad.indikt@gmail.com>
2020-03-20 11:18:50 +0200

    login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import marketApp from '../views/market-app.page'
+import loginSignup from '../views/login-signup-page'
 
 
 Vue.use(VueRouter)
@@ -9,7 +10,15 @@ const routes = [{
     path: '/',
     name: 'market-app',
     component: marketApp
-}, ]
+},
+{
+    path: '/login',
+    component: loginSignup
+},
+{
+    path: '/signup',
+    component: loginSignup
+} ]
 
 const router = new VueRouter({
     routes
--- src/views/login-signup-page.vue
+++ src/views/login-signup-page.vue
@@ -1,18 +1,58 @@
 <template>
   <section class=login-signup-container>
-      <section class="user-login">
+      <section v-if="!isSignup" class="user-login">
+            <h2>please login</h2>
             <form @submit.prevent="login">
-                <input type="text" placeholder="Name" v-model="credentials.username" />
+                <input type="text" placeholder="email" v-model="credentials.username" />
                 <input type="password" placeholder="Your Password" v-model="credentials.password" />
                 <button>Login</button>
             </form>
-        </section>
+      </section>
+      <section v-else class="user-signup">
+            <h2>please sign up</h2>
+            <form @submit.prevent="signup">
+                <input type="text" placeholder="Full name" v-model="credentials.fullName" />
+                <input type="text" placeholder="email" v-model="credentials.username" />
+                <input type="password" placeholder="Your Password" v-model="credentials.password" />
+                <button>Login</button>
+            </form>
+      </section>
   </section>
 </template>
 
 <script>
 export default {
   name:'login-signup-cmp',
+  data() {
+    return {
+      credentials: {
+        username: '',
+        password: ''
+      },
+      signupCreds: {
+        fullName: '',
+        username: '',
+        password: ''
+      },
+      isSignup: false
+    }
+  },
+   methods: {
+    login(credentials) {
+      this.$store.dispatch({
+        type: 'login', credentials
+      })
+    },
+    signup(signupCreds) {
+      this.$store.dispatch({
+        type: 'signup', signupCreds
+      })
+    },
+
+  },
+  created() {
+    if (this.$route.path === '/signup') this.isSignup = true;
+  }
 
 }
 </script>
