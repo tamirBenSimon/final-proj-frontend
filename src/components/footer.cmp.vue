f9d807835a92ce317900630e0b4caf22f30e4cef
Your Nameariel <arielzissu98@gmail.com>
2020-03-20 17:07:09 +0200

    footer+navbar

---
+++ src/components/footer.cmp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="footer">   
+        footer
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.footer{
+    background-color: rgb(168, 161, 161);
+    padding: 10px;
+}
+</style>
