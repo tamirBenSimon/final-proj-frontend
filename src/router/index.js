ad6d149238fd553b47a62873b306fb7a209de417
Vlad Indikt <vlad.indikt@gmail.com>
2020-03-21 16:40:29 +0200

    continue work on sellers gallery

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import loginSignup from '../views/login-signup-page';
 import admin from '../views/admin.page';
 import details from '../views/product-details.page';
 import cart from '../views/cart.page';
-
+import sellerGallery from '../views/seller-gallery.page';
 
 
 Vue.use(VueRouter)
@@ -43,6 +43,10 @@ const routes = [
     {
         path: '/cart',
         component: cart
+    },
+    {
+        path: '/sellerGallery/:id',
+        component: sellerGallery
     }
 ]
 
