e226f5d73acbf5b7f8860e2229d2408b81ef7a47
TamirBenSimon <tamirbssb@gmail.com>
2020-03-21 12:23:09 +0200

    saturday morning

--- src/components/product-prev.cmp.vue
+++ src/components/product-prev.cmp.vue
@@ -1,53 +1,56 @@
 <template>
-  <div class="artwork-prev">
-    <router-link :to="'/artwork/'+artwork._id">  
+  <router-link class="artwork-prev" :to="'/artwork/' + artwork._id">
     <div class="artwork-prev-img-container">
-      <img :src="Showartwork" alt="" />
-    </div>
-    <div class="artwork-prev-txt-container">
-      <div class="artwork-prev-txt-header">
-      <h3>{{ artwork.title }}</h3>
-      <span>{{ artwork.price }}USD</span> 
+      <img  :src="Showartwork" alt="" />
+      <div class="artwork-prev-txt-container">
+        <div class="artwork-prev-txt-header">
+          <h3>{{ artwork.title }}</h3>
+          <span>{{ artwork.price }}USD</span>
+        </div>
+        <span class="artwork-prev-createdBy-fullName">
+          {{ artwork.createdBy.fullName }}</span
+        >
+        <!-- <span>{{  artwork.desc }}</span> <br> -->
+        <span>{{ isInStock }}</span>
+        <div class="artwork-prev-controle-pad">
+          <button @click.prevent="remove">remove</button>
+        </div>
       </div>
-      <span class="artwork-prev-createdBy-fullName">{{ artwork.createdBy.fullName}}</span> <br />
-      <!-- <span>{{  artwork.desc }}</span> <br> -->
-      <span>{{ isInStock }}</span> 
-      <div class="artwork-prev-controle-pad">
-        <button @click.prevent="remove">remove</button>
-      </div>
     </div>
-    </router-link>
-  </div>
+  </router-link>
 </template>
 
 <script>
-import {eventBus, EVENT_REMOVE} from '../services/event-bus.service.js'
+import { eventBus, EVENT_REMOVE } from "../services/event-bus.service.js";
 export default {
   name: "product-prev",
   props: {
     artwork: Object
   },
-  data(){
+  data() {
     return {
-      imgUrlIdx:0
-    }
+      imgUrlIdx: 0
+    };
   },
-  mounted(){
-    setInterval(()=>{
-      this.imgUrlIdx= (this.imgUrlIdx===(this.artwork.imgURLs.length-1))? 0 :this.imgUrlIdx+1
-    },7000)
+  mounted() {
+    setInterval(() => {
+      this.imgUrlIdx =
+        this.imgUrlIdx === this.artwork.imgURLs.length - 1
+          ? 0
+          : this.imgUrlIdx + 1;
+    }, 7000);
   },
   computed: {
     isInStock() {
       return this.artwork.inStock ? "available" : "currently not available";
     },
-    Showartwork(){
+    Showartwork() {
       return this.artwork.imgURLs[this.imgUrlIdx];
     }
   },
-  methods:{
-    remove(){
-             eventBus.$emit(EVENT_REMOVE, this.artwork._id);
+  methods: {
+    remove() {
+      eventBus.$emit(EVENT_REMOVE, this.artwork._id);
     }
   }
 };
