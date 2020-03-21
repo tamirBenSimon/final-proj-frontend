5d0c8c2bef2ddda5cadf801267c0200fda0d53af
TamirBenSimon <tamirbssb@gmail.com>
2020-03-21 12:52:54 +0200

    12:52

--- src/components/artwork-filter.cmp.vue
+++ src/components/artwork-filter.cmp.vue
@@ -5,6 +5,7 @@
       <input type="number" v-model.number="filterBy.minPrice" />
       <input type="number" v-model.number="filterBy.maxPrice" />
       <input type="text" v-model="filterBy.tags" />
+      <input type="text" v-model="filterBy.creatorId" placeholder="artiist ID" />
       <button @click.prevent="onFilter">filter</button>
     </div>
     <pre>
