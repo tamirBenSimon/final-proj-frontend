d2403450f40fc70f5f4ea5deae4e1889deb51d38
TamirBenSimon <tamirbssb@gmail.com>
2020-03-23 13:58:14 +0200

    sc

--- src/views/home.page.vue
+++ src/views/home.page.vue
@@ -1,19 +1,24 @@
 <template>
   <section class="home-main-layout">
     <!-- <homeHeaderToApp></homeHeaderToApp> -->
-     <homeHeader2>
-    </homeHeader2> 
+    <homeHeader2> </homeHeader2>
+    <div class="tags-title">
+      <h3>Explore trending streams on {{ this.filterBy.tags }}</h3>
+    </div>
     <tags-select @tagClicked="tagClicked" :tags="homeTags"> </tags-select>
     <product-list :artworks="artworks" />
+    <div class="hero-comuunity">
+      <h1>hero</h1>
+    </div>
   </section>
 </template>
 
 <script>
 import productList from "../components/product-list.cmp";
 // import  homeHeaderToApp from "../components/home-header-to-app.cmp";
-import  tagsSelect  from "../components/tags-select.cmp";
-import  homeHeader2 from "../components/home-header2.cmp";
-import {eventBus, EVENT_REMOVE} from '../services/event-bus.service.js';
+import tagsSelect from "../components/tags-select.cmp";
+import homeHeader2 from "../components/home-header2.cmp";
+import { eventBus, EVENT_REMOVE } from "../services/event-bus.service.js";
 
 export default {
   name: "home-page",
@@ -23,34 +28,29 @@ export default {
     homeHeader2,
     tagsSelect
   },
-  data(){
-    return{
-      homeTags:["nature", "urban", "psychedelic", "art", "exhibit","go"]
-    }
+  data() {
+    return {
+      filterBy: { tags: "urban" },
+      homeTags: ["nature", "urban", "psychedelic", "art", "exhibit", "go"]
+    };
   },
   created() {
     this.$store.dispatch({
       type: "loadArtworks",
-      filterBy:{tags:'urban'}
+      filterBy: { tags: this.filterBy.tags }
     });
 
-    eventBus.$on(EVENT_REMOVE,(artworkId)=>{
-      this.removeArtwork(artworkId)})
+    eventBus.$on(EVENT_REMOVE, artworkId => {
+      this.removeArtwork(artworkId);
+    });
   },
-  methods:{
-        removeArtwork (artworkId){
+  methods: {
+    tagClicked(tag) {
+      this.filterBy.tags=tag
       this.$store.dispatch({
-      type: "removeArtwork",
-      artworkId
-    });
-    },
-
-    tagClicked(tag){
-      console.log(tag)
-            this.$store.dispatch({
-      type: "loadArtworks",
-      filterBy:{tags: tag}
-    });
+        type: "loadArtworks",
+        filterBy: { tags: this.filterBy.tags }
+      });
     }
   },
   computed: {
@@ -58,5 +58,5 @@ export default {
       return this.$store.getters.artworks;
     }
   }
-}
+};
 </script>
