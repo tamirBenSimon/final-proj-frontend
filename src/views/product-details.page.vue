7999ed0ec9ade7abce72e5b1b96076590c8be164
Your Nameariel <arielzissu98@gmail.com>
2020-03-23 18:48:25 +0200

    asda

--- src/views/product-details.page.vue
+++ src/views/product-details.page.vue
@@ -29,10 +29,14 @@
 
       <div class="product-details-aside-container">
         <div class="product-details-seller-main flex-center">
-          <img class="product-details-img-seller" :src="artwork.createdBy.imageURL" alt="Seller Picture">
+          <router-link :to="'/sellerGallery/'+artwork.createdBy._id">
+            <img class="product-details-img-seller" :src="artwork.createdBy.imageURL" alt="Seller Picture">
+          </router-link>
           <span class="product-details-span">|</span>
           <div class="product-details-seller-main2 flex-center">
+            <router-link :to="'/sellerGallery/'+artwork.createdBy._id">
             <h4 class="product-details-seller-name ">{{artwork.createdBy.fullName}}</h4>
+            </router-link>
             <span class="product-details-span">|</span>
             <h4 class="product-details-seller-sales">{{artwork.salesCount}} sales</h4>
           </div>
