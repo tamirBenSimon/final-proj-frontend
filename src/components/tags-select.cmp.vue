2fae93b2997c25364b929081f8bca4b82462ff6b
TamirBenSimon <tamirbssb@gmail.com>
2020-03-23 12:50:02 +0200

    op

---
+++ src/components/tags-select.cmp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tag-select" >
+      <span @click="onTagClicked(tag)" class="tag-select-span" v-for="(tag, idx) in tags" :key="idx">
+        {{ tag }}
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: Array
+  },
+  data() {
+    return {};
+  },
+  methods: {
+onTagClicked(tag){
+  this.$emit('tagClicked', tag)
+}
+
+  }
+};
+</script>
