979fc4294033e121f083b0c746e4bb0c44889a59
TamirBenSimon <tamirbssb@gmail.com>
2020-03-23 09:12:48 +0200

    mondayMorning

--- src/components/home-header2.cmp.vue
+++ src/components/home-header2.cmp.vue
@@ -1,30 +1,37 @@
 <template>
-    <header class="home-main-header2" >
+  <header class="home-main-header2">
     <!-- <img src="../../public/img/gallery/gallery2.jpg" alt="homepic"> -->
-    <video width="100%" autoplay >
-  <source :src="require('../../public/video/homepageVid.mp4')" type="video/mp4">
-  Your browser does not support HTML5 video.
-</video>
-    <!-- <div class="home-header2">
-      <button>
-        Begin exploring now...
-      </button>
-    </div> -->
-      </header>
+    <video height="90%" width="100%" autoplay loop>
+      <source
+        :src="require('../../public/video/homepage2.1.mp4')"
+        type="video/mp4"
+      />
+      Your browser does not support HTML5 video.
+    </video>
+    <div class="home-header2">
+      <div class="home-header2-txt">
+        <h2>Online inspire <span class="whiter">community.</span></h2>
+        <h2>simple interaction <span class="whiter">marketplace.</span></h2>
+        <h2>paintings, photography, <span class="whiter">design.</span> </h2>
+        <h2>gallerior. make <span class="whiter">creativity</span>  count.</h2>
+        <button>
+          <router-link to="/artwork"> 
+
+          Begin exploring now...
+          </router-link>
+        </button>
+      </div>
+    </div>
+  </header>
+  
 </template>
 
 <script>
-
 export default {
-  data(){
-    return{
-    }
+  data() {
+    return {};
   },
-  created(){
-
-  },
-  methods:{
-
-  }
-}
+  created() {},
+  methods: {}
+};
 </script>
