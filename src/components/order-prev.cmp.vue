2676c4de70d101d705fa3587eebc70212dc9736b
Vlad Indikt <vlad.indikt@gmail.com>
2020-03-27 15:35:09 +0300

    orders table

---
+++ src/components/order-prev.cmp.vue
@@ -0,0 +1,39 @@
+<template>
+    <tr>
+        <td>{{count+1}}</td>
+        <td>{{order.at}}</td>
+        <td><img :src="order.by.imgURL"  alt=""><div>{{order.by.fullName}}</div></td>
+        <td>{{order.product.title}}</td>
+        <td>{{order.product.price}}</td>
+        <td>{{order.status}}</td>
+        <td>{{order.shippingInfo}}</td>
+        <td><button @click="changeStatus(order.status)">change status</button></td>
+    </tr>
+</template>
+
+<script>
+// import {eventBus} from '../services/event-bus.service.js'
+
+export default {
+  name: 'order-prev',
+   props:{
+     order: Object,
+     count: Number
+   },
+   methods:{
+     changeStatus(orderStatus){
+       console.log('changing order something ',orderStatus)
+      //  eventBus.$emit('delete', userId);
+      }
+    }
+}
+</script>
+
+<style scoped>
+table, th, td {
+  border: 1px solid grey;
+  padding: 5px;
+  border-collapse: collapse;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import store from './store';
 import VuePictureSwipe from 'vue-picture-swipe';
 Vue.component('vue-picture-swipe', VuePictureSwipe);
 
+import ElementUI from 'element-ui'; //vue elementUI lib
+import 'element-ui/lib/theme-chalk/index.css'; //vue elementUI lib
+
 import './registerServiceWorker';
 import './styles/global.scss';
 
+Vue.use(ElementUI); //vue elementUI lib
 Vue.config.productionTip = false;
 
 new Vue({
