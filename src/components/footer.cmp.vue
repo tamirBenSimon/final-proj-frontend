d25a4ce3323f5f29ca0087c0b653d096e9099307
Your Nameariel <arielzissu98@gmail.com>
2020-03-28 23:30:01 +0300

    ariel

--- src/components/footer.cmp.vue
+++ src/components/footer.cmp.vue
@@ -1,12 +1,17 @@
 <template>
-  <section class="footer">
-    <h2>Galerior</h2>
+  <section class="footer-main">
+    <img @click="onLogo" class="footer-logo btn" src="../../public/img/icons/royal-logo.png" alt="Logo" title="Gallerior!"> 
     <h2 class="footer-line">Copyright &copy; 2020 Gallerior - All Rights Reserved</h2>
   </section>
 </template>
 
 <script>
 export default {
+  methods:{
+     onLogOut(){
+      this.$router.push('/');
+    }
+  }
 
 }
 </script>
