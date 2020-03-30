ffd31cec2331c3ebc0cbe1b5a824194a641eeaf3
TamirBenSimon <tamirbssb@gmail.com>
2020-03-30 17:20:34 +0300

    popo

--- src/components/product-prev.cmp.vue
+++ src/components/product-prev.cmp.vue
@@ -1,48 +1,78 @@
 <template>
   <router-link class="artwork-prev" :to="getURL">
-    
-      <transition name="el-fade-in-linear">
-    <div class="artwork-prev-img-container" shadow="hover">
-      <div class="artwork-prev-wish-list">
-        <img v-if="!isWishlist" @click.prevent="onWishlist" class="artwork-prev-wish-list-img" src="../../public/img/icons/black-like.png" alt="Wish List">
-        <img v-else @click.prevent="onWishlist" class="artwork-prev-wish-list-img" src="../../public/img/icons/red-like.png" alt="Wish List">
-      </div>
+    <transition name="el-fade-in-linear">
+      <div class="artwork-prev-img-container" shadow="hover">
 
-      <div class="artwork-prev-txt-container flex-center">
 
+        <div class="artwork-prev-txt-container flex-center">
           <img class="artwork-prev-img" :src="Showartwork" alt="Artwork" />
 
           <div class="artwork-prev-container-flach">
             <div class="artwork-prev-txt-header">
-              <h3>{{ artwork.title }}</h3>
-              <h4><span class="artwork-prev-USD">$</span>{{ artwork.price }}</h4>
+              <h4>{{ artwork.title }}</h4>
+              <h4>
+                <span class="artwork-prev-USD">$</span>{{ artwork.price }}
+              </h4>
             </div>
-            <router-link :to="'/sellerGallery/'+artwork.createdBy._id">
+
+
+            <router-link :to="'/sellerGallery/' + artwork.createdBy._id">
+
               <div class="artwork-prev-created-by">
-                <img class="artwork-prev-cretadBy-img" :src="artwork.createdBy.imgURL" alt="">
+                <img
+                  class="artwork-prev-cretadBy-img"
+                  :src="artwork.createdBy.imgURL"
+                  alt=""
+                />
+
                 <span class="artwork-prev-createdBy-fullName">
                   {{ artwork.createdBy.fullName }}
                 </span>
 
-                  <i v-if="!isWishlist" @click.prevent="onWishlist"  class="fas fa-heart artwork-prev-wishlist-icon"></i>
-                  <i v-else @click.prevent="onWishlist"  class="fas fa-heart artwork-prev-wishlist-icon-added"></i>
+                <div class="prev-wishlist-container">
+                  <el-popover
+                    v-if="!isWishlist"
+                    @click.prevent="onWishlist"
+                    placement="bottom-end"
+                    width="20px"
+                    trigger="hover"
+                    content="Add to wishlist"
+                  >
+                    <i
+                      slot="reference"
+                      @click.prevent="onWishlist"
+                      class="fas fa-heart artwork-prev-wishlist-icon"
+                    ></i>
+                  </el-popover>
+                  <el-popover
+                    v-else
+                    @click.prevent="onWishlist"
+                    placement="bottom-end"
+                    width="20px"
+                    trigger="hover"
+                    content="Remove from wishlist"
+                  >
+                    <i
+                      class="fas fa-heart artwork-prev-wishlist-icon-added"
+                      slot="reference"
+                      @click.prevent="onWishlist"
+                    ></i>
+                  </el-popover>
 
+                </div>
               </div>
             </router-link>
             <div class="artwork-prev-controle-pad">
+              <i class="el-icon-delete" @click.prevent="remove"></i>
 
-                <i class="el-icon-delete"  @click.prevent="remove"></i>
-
-              <router-link :to="'/artwork/edit/'+ this.artwork._id">
-              <i  class="el-icon-edit"></i>
-               </router-link>
+              <router-link :to="'/artwork/edit/' + this.artwork._id">
+                <i class="el-icon-edit"></i>
+              </router-link>
             </div>
           </div>
-
+        </div>
       </div>
-
-    </div>
-          </transition>
+    </transition>
   </router-link>
 </template>
 
@@ -60,12 +90,12 @@ export default {
       isWishlist: false
     };
   },
-  created(){
+  created() {
     this.loggedinUser = this.$store.getters.loggedinUser;
   },
   computed: {
-    getURL(){
-      return  ('/artwork/' + this.artwork._id)
+    getURL() {
+      return "/artwork/" + this.artwork._id;
     },
     isInStock() {
       return this.artwork.inStock ? "available" : "currently not available";
@@ -73,7 +103,7 @@ export default {
     Showartwork() {
       return this.artwork.imgURLs[this.imgUrlIdx];
     },
-    getSrc(){
+    getSrc() {
       return this.currSrc;
     }
   },
@@ -81,12 +111,12 @@ export default {
     remove() {
       eventBus.$emit(EVENT_REMOVE, this.artwork._id);
     },
-    onWishlist(){
+    onWishlist() {
       const userId = this.loggedinUser._id;
       const product = this.artwork;
-      eventBus.$emit('addWishlist', userId, product);
+      eventBus.$emit("addWishlist", userId, product);
       this.isWishlist = !this.isWishlist;
     }
   }
-}
+};
 </script>
